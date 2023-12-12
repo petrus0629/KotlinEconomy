410969d24cd8f408ddac0debe97be73dc5833924
petrus0629 <59354462+petrus0629@users.noreply.github.com>
2023-12-12 22:21:00 +0900

    Initial commit

---
+++ src/main/java/org/example/kot/kotlineconomy/KotlinEconomy.kt
@@ -0,0 +1,14 @@
+package org.example.kot.kotlineconomy
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KotlinEconomy : JavaPlugin() {
+    override fun onEnable() {
+        
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
